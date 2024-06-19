6f3cacec252df157d22bdd01d9d0be9f727a11d7
Maina, Stephen <m41na@yahoo.com>
2024-06-18 23:12:11 -0500

    url quick update - 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://akilisha.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/karanunganga/build/',
+  baseUrl: '/karanunganga/build/index.html',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
